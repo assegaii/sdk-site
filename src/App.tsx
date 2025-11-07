03959829afbfafa1dc06a2e39e3ad7bae23351b4
Abusarome <71697564+Abusarome@users.noreply.github.com>
2025-11-07 23:18:34 +0700

    hotfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import MainContent from './components/MainContent/MainContent'
