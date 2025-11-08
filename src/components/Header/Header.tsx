0375ed08e796c9955fc803540be7adc2ffa6df16
Abusarome <71697564+Abusarome@users.noreply.github.com>
2025-11-08 20:23:30 +0700

    add product section

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Header.module.css';
 
 import logo from '../../assets/logo.png';
-import shopIcon from '../../assets/shopIcon.png';
+import shopIcon from '../../assets/icons/shopIcon.png';
 
 
 const Header: React.FC = () => {
--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,58 +1,79 @@
 import React from 'react';
 import styles from './MainContent.module.css';
 
-import QualityIcon from '../../assets/quality-icon.png';
-import PricesIcon from '../../assets/prices-icon.png';
-import AdviceIcon from '../../assets/advice-icon.png';
-import DeliveryIcon from '../../assets/delivery-icon.png';
-
 import FeedbackCarousel from '../FeedbackCarousel/FeedbackCarousel';
+
+import Plank from '../../assets/product-image/plank.png';
+import Plywood from '../../assets/product-image/plywood.png';
+import RoundWood from '../../assets/product-image/round-wood.png';
+import Veneer from '../../assets/product-image/veneer.png';
+import WoodChips from '../../assets/product-image/wood-chips.png';
+import AboutUs from '../AboutUs/AboutUs';
+
 
 const MainContent: React.FC = () => {
     return (
         <main className={styles.mainContent}>
 
             <FeedbackCarousel />
-            <section className={styles.featuresSection}>
+            <AboutUs />
+            <h1 className={styles.productSectionName}>
+                Продукция
+            </h1>
+            <section className={styles.productSection}>
                 <div className={styles.container}>
-                    <h2 className={styles.sectionTitle}>ПОЧЕМУ ВЫБИРАЮТ НАС</h2>
-                    <div className={styles.featuresGrid}>
-                        <div className={styles.featureCard}>
-                            <img src={QualityIcon} className={styles.featureIconQuality} />
-                            <div className={styles.featureText}>
-                                <h3>Качество, проверенное временем</h3>
-                                <p>Мы работаем только с сертифицированными поставщиками и контролируем качество каждой партии.</p>
+                    <div className={styles.productGrid}>
+                        <div className={styles.productCard}>
+                            <div className={styles.cardImageContainer}>
+                                <img src={Plank} alt="Доска" className={styles.productIconPlank} />
+                            </div>
+                            <div className={styles.productDesc}>
+                                <h2 className={styles.productTitle}>Доска</h2>
+                                <p>Типа какое-то описание чтоли</p>
+                                <button className={styles.sizeBtn}>Размеры</button>
                             </div>
                         </div>
-                        <div className={styles.featureCard}>
-                            <img src={PricesIcon} className={styles.featureIconPrices} />
-                            <div className={styles.featureText}>
-                                <h3>Индивидуальные условия для партнёров</h3>
-                                <p>Персональные скидки и гибкая система оплаты.</p>
+                        <div className={styles.productCard}>
+                            <div className={styles.cardImageContainer}>
+                                <img src={RoundWood} alt="Круглый лес" className={styles.productIconRoundWood} />
+                            </div>
+                            <div className={styles.productDesc}>
+                                <h2 className={styles.productTitle}>Круглый лес</h2>
+                                <p>Типа какое-то описание чтоли</p>
+                                <button className={styles.sizeBtn}>Размеры</button>
                             </div>
                         </div>
-                        <div className={styles.featureCard}>
-                            <img src={AdviceIcon} className={styles.featureIconAdvice} />
-                            <div className={styles.featureText}>
-                                <h3>Профессиональная консультация</h3>
-                                <p>Помогаем подобрать оптимальные материалы под ваш проект.</p>
+                        <div className={styles.productCard}>
+                            <div className={styles.cardImageContainer}>
+                                <img src={WoodChips} alt="Щепа" className={styles.productIconWoodChips} />
+                            </div>
+                            <div className={styles.productDesc}>
+                                <h2 className={styles.productTitle}>Щепа</h2>
+                                <p>Типа какое-то описание чтоли</p>
+                                <button className={styles.sizeBtn}>Размеры</button>
                             </div>
                         </div>
-                        <div className={styles.featureCard}>
-                            <img src={DeliveryIcon} className={styles.featureIconDelivery} />
-                            <div className={styles.featureText}>
-                                <h3>Работаем по всей России</h3>
-                                <p>Доставка в любые регионы собственным транспортом.</p>
+                        <div className={styles.productCard}>
+                            <div className={styles.cardImageContainer}>
+                                <img src={Plywood} alt="Фанера" className={styles.productIconPlywood} />
+                            </div>
+                            <div className={styles.productDesc}>
+                                <h2 className={styles.productTitle}>Фанера</h2>
+                                <p>Типа какое-то описание чтоли</p>
+                                <button className={styles.sizeBtn}>Размеры</button>
+                            </div>
+                        </div>
+                        <div className={styles.productCard}>
+                            <div className={styles.cardImageContainer}>
+                                <img src={Veneer} alt="Шпон" className={styles.productIconVeneer} />
+                            </div>
+                            <div className={styles.productDesc}>
+                                <h2 className={styles.productTitle}>Шпон</h2>
+                                <p>Типа какое-то описание чтоли</p>
+                                <button className={styles.sizeBtn}>Размеры</button>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
-            <section>
-                <div>
-                    <h1>
-                        Продукция
-                    </h1>
                 </div>
             </section>
         </main>
